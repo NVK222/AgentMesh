e1d613fa64ea243ad1355e55bd6b05259669246c
NVK222 <neerajvk2004@gmail.com>
2026-05-23 17:38:22 +0530

    Remove unneeded app api

--- apps/api/src/routes/mission.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createMission, getMissions } from "../controllers/mission.controller";
-import asyncHandler from "../utils/asyncHandler";
-
-const missionRouter = Router();
-missionRouter.get("/", asyncHandler(getMissions));
-missionRouter.post("/", asyncHandler(createMission));
-
-export default missionRouter;
